ca1131f8691cf379790fdb5eb558ff62f174427b
danben1 <124446045+danben1@users.noreply.github.com>
2023-03-19 21:50:13 +0000

    included examples on how to control user input using regular expressions and how to alter probability of computer generating an outcome using for loops and if statements

--- library.js
+++ library.js
@@ -80,3 +80,75 @@
         words[i] = `${words[i]}t`
     }
     console.log(words);
+
+//restrict user input to character limit and capitalization using regular expressions
+    //while loop begins when variable 'userName' does NOT:
+        //string begin with a capital --> ^[A-Z]
+        //string ends with 0-9 characters, upper or lower case --> [a-zA-z]{0,9}$
+    //.test command used to check compliance with regular expression /^[A-Z][a-zA-Z]{0,9}$/
+    
+    while (!/^[A-Z][a-zA-Z]{0,9}$/.test(userName));
+
+    //syntax explaines
+        // ! --> defines that test variable should NOT equal content of regular expression
+        // /.../ --> forward ticks used to delimit extent of regular expression, they do not form part of expression itself
+        // ^ --> means start of string, combined with condition of capital [A-Z], means string must start with capital
+        //[a-zA-Z][0,9] --> must contains 0 to 9 character upper or lower case 
+        // $ --> denotes the end of the string, in instance above condition set is that must end with 0-9 characters as outlined above
+
+//FOR LOOP TO ADJUST PROBABILITY OF A RANDOMISED RESULT e.g. make computer win RPS 90% of time.
+    // instead of many if statements create a 'matrix' which can be used to determine the winner of any given moves
+    //create an array of arrays
+ 
+
+    let outcomes = 
+    [
+        ["drawn", "lost", "won", "won", "lost"], // if humanMove=rock;    computerMove=rock, paper, scissors, lizard, spock
+        ["won", "drawn", "lost", "lost", "won"], // humanMove=paper;
+        ["lost", "won", "drawn", "won", "lost"], // humanMove=scissors; 
+        ["lost", "won", "lost", "drawn", "won"], // humanMove=lizard;
+        ["won", "lost", "won", "lost", "drawn"], // humanMove=spock;
+    ];
+​
+    //have computer win ALL the time 
+    //create for loop that will iterate through matrix until it reaches a "won" scenario
+    //then returns that win scenario as its output
+    
+    let computerMove;   //decalre undefined variable
+    let playerMove = 2  //as an example assume player selects 2 ("scissors")
+    
+    for (let i = 0; i < outcomes.length; i++) {     //define for loop to iterate through
+        if(outcomes[playerMove][i] == "lost")       //navigate to 2nd index position (scissors array) and iterate through till find "lost" condition
+         {
+             computerMove = i;                      //return index position that generated a loss condition as computer move
+         }
+    };
+    
+    let result = outcomes[playerMove][computerMove]; //the result would then always be a playermove loss
+
+    //have the computer win a certain percentage of the time
+    //generate a random number betwwen 0 and 10
+
+    let randomNumber = Math.floor(Math.random() * 11);
+    
+    //create an if statement that regulates how often the computer should win
+    //change the number here to change the percentage of wins e.g. 5 would be under 50% of time
+    
+    if (randomNumber < 5)                                   //50% of time rig answer using for loop shown above
+    {
+        for (let i = 0; i < outcomes.length; i++)
+        {
+            if(outcomes[playerMove][i] == "lost")
+            {
+                computerMove = i;
+            }
+        };
+        result = outcomes[playerMove][computerMove]; 
+    }
+    else                                                    //else allow play to continue randomly (not rigged)
+    {
+        computerMove = Math.floor(Math.random() * outcomes.length);     //generate random number for computerMove rather than engineering a winning pick
+        result = outcomes[playerMove][computerMove];                    //allow result to play out normally
+    };
+
+
